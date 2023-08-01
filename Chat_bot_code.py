c96da4655433d720333d26595164139dd6beeba0
BasudevKar-uni <140535602+BasudevKar-uni@users.noreply.github.com>
2023-08-02 01:12:22 +0530

    Update Chat_bot_code.py

--- Chat_bot_code.py
+++ Chat_bot_code.py
@@ -1,14 +1,17 @@
 import json
 from difflib import get_close_matches
 
+
 def load_knowledge_base(file_path: str):
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
     return data
 
+
 def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
+
 
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
@@ -21,15 +24,15 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
             return q["answer"]
     return None
 
+
 def chatbot():
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
     while True:
         user_input: str = input("You: ")
 
-        if user_input.lower() == 'bye' / 'end':
+        if user_input.lower() == 'bye'  'end':
             break
-
 
         best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
 
